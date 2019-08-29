245b50a10070a8c408b036e1bdad81a1728133fa
d-coder <varunp.karhadkar@gmail.com>
2019-08-30 03:47:04 +0530

    adding tests

---
+++ app/src/androidTest/java/com/apps/demo/apixuweather/APITest.java
@@ -0,0 +1,29 @@
+package com.apps.demo.apixuweather;
+
+
+import androidx.test.runner.AndroidJUnit4;
+import com.apps.demo.apixuweather.view.MainActivity;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4.class)
+public class APITest {
+
+
+//    @Rule
+//    public ActivityTestRule<MainActivity> mActivityRule =
+//            new ActivityTestRule<>(MainActivity.class, true, false);
+//    private MockWebServer server;
+//
+//    @Before
+//    public void setUp() throws Exception {
+//        super.setUp();
+//        server = new MockWebServer();
+//        server.start();
+//        injectInstrumentation(InstrumentationRegistry.getInstrumentation());
+//        QuoteOfTheDayConstants.BASE_URL = server.url("/").toString();
+//    }
+
+
+}
